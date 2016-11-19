6c3b74aaf827efbb0d4843f4c45c2fd62a68fdec
Issam Tahiri <issam.tahiri@u-bordeaux.fr>
2016-11-19 15:03:07 +0100

    BenchmarkUtil is now operational

---
+++ src/BenchmarkUtils.jl
@@ -0,0 +1,175 @@
+module BenchmarkUtils
+
+using Cairo, Gadfly, Colors
+
+export run_test_compaign
+
+function generateCombinations(optionslist, ret, depth , current)
+    if depth == length(optionslist) + 1
+        push!(ret, current)
+        return
+    end
+
+    for i in range(1 , length(optionslist[depth]))
+        generateCombinations(optionslist, ret, depth+1 , [current..., optionslist[depth][i]])
+    end
+end
+
+# using Plots.(deprecated)
+# function plotMetric(dpoints, metric_name, folder)
+# # dpoints is a 2D np array (for a specific metric). each line has cat, conds, value
+#
+#     # init Plots backend.
+#     pyplot()
+#
+#     # the space between each set of bars
+#     space = 0.3
+#
+#     categories = convert(Array{String,1}, unique(dpoints[:,1]) )
+#     conditions = convert(Array{String,1}, unique(dpoints[:,2]) )
+#
+#     # @show categories
+#     # @show conditions
+#
+#     n = length(conditions)
+#     nCategories = length(categories)
+#
+#     # the width of each single bar. (1-space) is the width taken by all bars of one category
+#     width = (1 - space) / (length(conditions))
+#
+#     indeces = collect(1:length(categories))
+#
+#     #show(false) # doesn t work I dont know why
+#
+#     max_y = 0.0
+#     for (i,cond) in enumerate(conditions)
+#         # @show cond
+#         vals = Array{Float64,1}()
+#         for k in 1:size(dpoints, 1)
+#             dpoints[k,2] == cond && push!(vals, dpoints[k,3])
+#         end
+#         # @show vals
+#
+#         pos = [(j - (1 - space) / 2.0 + (i-0.5) * width) for j in indeces]
+#         # @show pos
+#
+#         bar!(pos, vals, label=cond, bar_width=width, reuse=(i==1?false:true) )
+#         max_y = max(max_y , maximum(vals))
+#     end
+#
+#     # @show nCategories+1.1
+#     #xlabel!("Plot Title")
+#     xlims!(1-1.1,nCategories+1.1),
+#     xticks!(indeces, categories)
+#
+#     ylabel!(metric_name)
+#     ylims!(0,max_y*1.6)
+#
+#     savefig(string(folder,metric_name,".pdf") )
+# end
+
+function gen_colors(n)
+  cs = distinguishable_colors(n,
+      [colorant"#FE4365", colorant"#eca25c"], # seed colors
+      lchoices=Float64[58, 45, 72.5, 90],     # lightness choices
+      transform=c -> deuteranopic(c, 0.1),    # color transform
+      cchoices=Float64[20,40],                # chroma choices
+      hchoices=[75,51,35,120,180,210,270,310] # hue choices
+  )
+end
+
+function plotMetric(dpoints, metric_name, folder)
+# dpoints is a 2D np array (for a specific metric). each line has cat, conds, value
+
+    # the space between each set of bars
+    space = 0.3
+
+    categories = convert(Array{String,1}, unique(dpoints[:,1]) )
+    conditions = convert(Array{String,1}, unique(dpoints[:,2]) )
+
+    n = length(conditions)
+    cond_colors = gen_colors(n)
+    nCategories = length(categories)
+
+    # the width of each single bar. (1-space) is the width taken by all bars of one category
+    width = (1 - space) / (length(conditions))
+    indeces = collect(1:length(categories))
+    max_y = 0.0
+    p = 0 # I don't know how to create an empty plot with Gadfly
+    for (i,cond) in enumerate(conditions)
+        # @show cond
+        vals = Array{Float64,1}()
+        for k in 1:size(dpoints, 1)
+            dpoints[k,2] == cond && push!(vals, dpoints[k,3])
+        end
+        # @show vals
+
+        pos = [(j - (1 - space) / 2.0 + (i-0.5) * width) for j in indeces]
+        # @show pos
+
+        if i == 1
+            p = plot(xmin=pos .- (width/2.0), xmax=pos .+ (width/2.0), y=vals, Geom.bar,
+                    Theme(default_color=cond_colors[i]) )
+        else
+            l = layer(xmin=pos .- (width/2.0), xmax=pos .+ (width/2.0), y=vals, Geom.bar,
+                    Theme(default_color=cond_colors[i]) )
+            push!(p,l)
+        end
+
+        max_y = max(max_y , maximum(vals))
+    end
+
+    push!(p, Guide.manual_color_key("", conditions, cond_colors) )
+    push!(p, Coord.Cartesian(xmin=1-1.1, xmax=nCategories+1.1))
+    push!(p, Guide.ylabel(metric_name))
+    push!(p, Guide.xlabel(string([string(i,":",cat,"   ") for (i,cat) in enumerate(categories)]...)))
+    push!(p, Guide.xticks(ticks=indeces))
+
+    draw(PDF(string(folder,metric_name,".pdf"), 6inch, 4inch), p)
+end
+
+
+function run_test_compaign(categories, params, params_values, test_func, metrics, folder)
+
+    params_values_list = []
+    for p in params
+        push!(params_values_list , params_values[p])
+    end
+
+    combs=[]
+    generateCombinations(params_values_list, combs, 1, [])
+    # @show combs
+
+    dpoints_array = [[] for i in 1:length(metrics)]
+    for cat in categories
+        for comb in combs
+            cond = ""
+            for (i,p) in enumerate(params)
+                cond = string(cond, p, comb[i])
+                if i != length(params)
+                    cond *= "-"
+                end
+            end
+            result = test_func(cat, comb...)
+            for (i, met_val) in enumerate(result)
+                # @show i
+                push!(dpoints_array[i], [string(cat) cond met_val])
+                # @show dpoints_array
+            end
+        end
+    end
+    # @show dpoints_array
+
+    for (i, met_dpoints) in enumerate(dpoints_array)
+        # @show met_dpoints
+        met_dpoints_2D = vcat(met_dpoints...)
+        metric_name = string(metrics[i])
+        @show (met_dpoints_2D, metric_name)
+        plotMetric(met_dpoints_2D, metric_name, folder)
+    end
+end
+
+
+end # module
+
+
---
+++ test/runtests.jl
@@ -0,0 +1,73 @@
+#using BenchmarkUtils
+
+#using Plots
+
+include("../src/BenchmarkUtils.jl")
+
+function test_func_example(cat, s, p)
+    if cat == :A
+        return s + p , s * p
+    else
+        return 2 + s + p , 2 * s * p
+    end
+end
+
+categories = [:A, :B]
+params = [:s, :p]
+params_values = Dict(:s => [1,2], :p => [3,4])
+metrics = [:SumMetic, :ProdMetric]
+folder = "/Users/itahiri/GIT_REP/"
+
+run_test_compaign(categories, params, params_values, test_func_example, metrics, folder)
+
+# y=[3, 7, 5, 1]
+# x=[1, 2, 3, 4]
+# xmin1 = x .- 0.1
+# xmax1 = x .+ 0.1
+#
+# xmin2 = x .- 0.5
+# xmax2 = x .- 0.3
+#
+# function gen_colors(n)
+#   cs = distinguishable_colors(n,
+#       [colorant"#FE4365", colorant"#eca25c"], # seed colors
+#       lchoices=Float64[58, 45, 72.5, 90],     # lightness choices
+#       transform=c -> deuteranopic(c, 0.1),    # color transform
+#       cchoices=Float64[20,40],                # chroma choices
+#       hchoices=[75,51,35,120,180,210,270,310] # hue choices
+#   )
+# end
+#
+# mycolors = gen_colors(5)
+#
+#
+# p = plot(#xmin=xmin1, xmax=xmax1,
+#     x=["A","B","C","D"], y=[3, 7, 5, 1], Geom.bar,
+#     xmin=xmin1, xmax=xmax1,
+#     Theme(default_color=mycolors[1]) )
+#
+# # The following creates a new layer that keeps the same color
+# #push!(p, layer(xmin=xmin2, xmax=xmax2, y=[3, 7, 5, 1], Geom.bar, order=2))
+#
+# # I do not want to use this because I don't know how many layers I will have
+#
+# # p = plot(x=[], y=[])
+# #
+# # push!(p, layer(xmin=xmin1, xmax=xmax1, y=[3, 7, 5, 1], Geom.bar,
+# #     Theme(default_color=mycolors[1])   ))
+# #
+# # push!(p, layer(xmin=xmin2, xmax=xmax2, y=[3, 7, 5, 1], Geom.bar,
+# #     Theme(default_color=mycolors[2])   ))
+# #
+# # push!(p, Guide.manual_color_key("", ["Thing One", "Thing Two"], [mycolors[1],mycolors[2]]) )
+#
+#
+# # define a plot
+# # myplot = plot(..)
+# # draw(PDF("myplot.pdf", 4inch, 3inch), myplot)
+
+
+
+
+
+
